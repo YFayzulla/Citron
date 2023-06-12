d356f5666b8d1e86333d6495e16e8ef5185920fb
YoldashevFayzulla <yoldashevfayzulla0@gmail.com>
2023-06-12 22:25:16 +0500

    template changed Azamat be carefully look .!

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -59,7 +59,7 @@
 <header id="header" class="d-flex align-items-center">
     <div class="container d-flex align-items-center justify-content-between">
 
-        <h1 class="logo"><a href="index.html">Day</a></h1>
+        <h1 class="logo"><a href="index.html">CiTroN</a></h1>
         <!-- Uncomment below if you prefer to use an image logo -->
         <!-- <a href="index.html" class="logo"><img src="assets/img/logo.png" alt="" class="img-fluid"></a>-->
 
@@ -71,9 +71,14 @@
                 <li><a class="nav-link scrollto " href="#portfolio">Portfolio</a></li>
                 <li><a class="nav-link scrollto" href="#pricing">Pricing</a></li>
                 <li><a class="nav-link scrollto" href="#team">Team</a></li>
-                <li class="dropdown"><a href="#"><span>Drop Down</span> <i class="bi bi-chevron-down"></i></a>
-                </li>
                 <li><a class="nav-link scrollto" href="#contact">Contact</a></li>
+                <li class="dropdown"><a href="#"><span>Language</span> <i class="bi bi-chevron-down"></i></a>
+                    <ul>
+                        <li><a href="#">Englsh</a></li>
+                        <li><a href="#">Руский</a></li>
+                        <li><a href="#">Uzbekcha</a></li>
+                    </ul>
+                </li>
             </ul>
             <i class="bi bi-list mobile-nav-toggle"></i>
         </nav><!-- .navbar -->
@@ -85,7 +90,7 @@
 <section id="hero" class="d-flex align-items-center">
     <div class="container position-relative" data-aos="fade-up" data-aos-delay="500">
         <h1>Welcome to Day</h1>
-        <h2>We are team of talented designers making websites with Bootstrap</h2>
+        <h2>We are team of talented designers making websites with Azamat</h2>
         <a href="#about" class="btn-get-started scrollto">Get Started</a>
     </div>
 </section><!-- End Hero -->
