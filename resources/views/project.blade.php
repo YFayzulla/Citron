4f594dcf244fbdb0a868d490c68fc5de45442ec0
YoldashevFayzulla <yoldashevfayzulla0@gmail.com>
2023-08-21 23:10:53 +0500

    project.blade.php editing

--- resources/views/project.blade.php
+++ resources/views/project.blade.php
@@ -5,37 +5,40 @@
     <section id="projects" class="services">
         <div class="container">
 
-                <div class="section-title">
-                    <span>{{__('index.Project')}}</span>
-                    <h2>{{__('index.Project')}}</h2>
-                    <p>{{__('index.Completed_Project')}}</p>
-                </div>
-{{--            @dd($users)--}}
+            <div class="section-title">
+                <span>{{__('index.Project')}}</span>
+                <h2>{{__('index.Project')}}</h2>
+                <p>{{__('index.Completed_Project')}}</p>
+            </div>
+            {{--            @dd($users)--}}
 
-                <div class="row">
-                    @foreach($projects as $project)
-                        <div class="col-lg-4 col-md-6 portfolio-item filter-app">
-                            <div class="card border-2">
+            <div class="row">
+                @foreach($projects as $project)
 
+                    <div class="col-lg-4 col-md-6 portfolio-item filter-app">
+                        <button type="button">
                             <img src="Aphoto/{{$project->image}}" class="img-thumbnail" alt="">
                             <div class="portfolio-info">
                                 <center>
-                                <h4 class="container">{{($project->{'name_'.app()->getLocale()})}}</h4>
+                                    <h4 class="container">{{($project->{'name_'.app()->getLocale()})}}</h4>
                                 </center>
                                 <p class="mt-2">{{($project->{'desc_' . app()->getLocale()})}}</p>
-                                @php $users=DB::table('project_has_user')->where('project_id', '=', $project->id)->get(); @endphp
-
-                                <p class="text-center"> {{__('index.loyixa_ishtirokchilari')}} </p>
-                                @foreach($users as $user)
-                                    <td class="px-4 py-3 text-sm">{{$user->user_id}}</td>
-                                @endforeach
                             </div>
-                        </div>
-                        </div>
-                    @endforeach
-                </div>
+                        </button>
+                    </div>
+                @endforeach
+            </div>
         </div>
     </section>
     <!-- End Projects Section -->
 
 @endsection
+
+{{--for modal--}}
+
+{{--@php $users=DB::table('project_has_user')->where('project_id', '=', $project->id)->get(); @endphp--}}
+
+{{--<p class="text-center"> {{__('index.loyixa_ishtirokchilari')}} </p>--}}
+{{--@foreach($users as $user)--}}
+{{--    <td class="px-4 py-3 text-sm">{{$user->user_id}}</td>--}}
+{{--@endforeach--}}
