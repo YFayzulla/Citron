45c82dec17dcff17be8f2006b0964b8494756c59
AZAMAT <azamatrahimov80@gmail.com>
2023-08-24 21:26:48 +0500

    updated service and about

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -9,9 +9,9 @@
     <meta content="" name="description">
     <meta content="" name="keywords">
 
-    <!-- Favicons -->
-    {{--    <link href="assets/img/favicon.png" rel="icon">--}}
-    {{--    <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">--}}
+{{--    <!-- Favicons -->--}}
+{{--        <link href="{{asset('assets/img/favicon.png')}}" rel="icon">--}}
+{{--        <link href="{{asset('assets/img/apple-touch-icon.png')}}" rel="apple-touch-icon">--}}
 
     <!-- Google Fonts -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
@@ -21,8 +21,8 @@
 
     <!-- Vendor CSS Files -->
     <link href="{{asset('assets/vendor/aos/aos.css')}}" rel="stylesheet">
-    {{--    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">--}}
-    {{--    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">--}}
+{{--        <link href="{{asset('assets/vendor/bootstrap/css/bootstrap.min.css')}}" rel="stylesheet">--}}
+{{--        <link href="{{asset('assets/vendor/bootstrap-icons/bootstrap-icons.css')}}" rel="stylesheet">--}}
     <link href="{{asset('assets/vendor/boxicons/css/boxicons.min.css')}}" rel="stylesheet">
     <link href="{{asset('assets/vendor/glightbox/css/glightbox.min.css')}}" rel="stylesheet">
     <link href="{{asset('assets/vendor/swiper/swiper-bundle.min.css')}}" rel="stylesheet">
