f5aa6638f5a1252e2c652b171e1b2f57412a0a87
AZAMAT <azamatrahimov80@gmail.com>
2023-09-05 20:57:06 +0500

    create mobile-nav-toggle

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -112,7 +112,7 @@
                     </ul>
                 </li>
             </ul>
-            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-list mobile-nav-toggle" viewBox="0 0 16 16">
+            <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="currentColor" class="bi bi-list mobile-nav-toggle" viewBox="0 0 16 16">
                 <path fill-rule="evenodd" d="M2.5 12a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5zm0-4a.5.5 0 0 1 .5-.5h10a.5.5 0 0 1 0 1H3a.5.5 0 0 1-.5-.5z"/>
             </svg>
         </nav><!-- .navbar -->
