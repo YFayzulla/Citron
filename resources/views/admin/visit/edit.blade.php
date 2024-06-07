d46bf335ed12acf5f3943bbf98979aa2124fcbea
YFayzulla <yoldashevFayzulla0@gmail.com>
2024-06-07 17:54:50 +0500

    edit

--- resources/views/admin/visit/edit.blade.php
+++ resources/views/admin/visit/edit.blade.php
@@ -21,65 +21,77 @@
                 <h1
                     class="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200"
                 >
-                    Create About
+                    Edit Visit
                 </h1>
                 @foreach ($errors->all() as $error)
                     <div>{{ $error }}</div>
                 @endforeach
-                <form method="post" action="{{route('abouts.update',$about->id)}}" enctype="multipart/form-data">
+                <form method="post" action="{{route('visit.update',$visit->id)}}" enctype="multipart/form-data">
                     @csrf
                     @method('PUT')
                     <label class="block text-sm">
-                        <span class="text-gray-700 dark:text-gray-400">Ismi</span>
-                        <input type="text" name="name" value="{{$about->name}}"
+                        <span class="text-gray-700 dark:text-gray-400">Country uzb name</span>
+                        <input type="text" name="country_uz" value="{{ $visit->country_uz }}"
+                               class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                        />
+                    </label>
+
+                    <label class="block text-sm">
+                        <span class="text-gray-700 dark:text-gray-400">Country rus name</span>
+                        <input type="text" name="country_ru" value="{{ $visit->country_ru }}"
+                               class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                        />
+                    </label>
+
+                    <label class="block text-sm">
+                        <span class="text-gray-700 dark:text-gray-400">Country eng name</span>
+                        <input type="text" name="country_en" value="{{ $visit->country_en }}"
+                               class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                        />
+                    </label>
+
+                    <label class="block text-sm">
+                        <span class="text-gray-700 dark:text-gray-400">University uzb name</span>
+                        <input type="text" name="university_uz_en" value="{{ $visit->university_uz_en }}"
                                class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
                         />
                     </label>
                     <label class="block text-sm">
-                        <span class="text-gray-700 dark:text-gray-400">Soxasining o'zbekcha nomi</span>
-                        <input type="text" name="specialty_uz" value="{{$about->specialty_uz}}"
+                        <span class="text-gray-700 dark:text-gray-400">University rus name</span>
+                        <input type="text" name="university_ru" value="{{ $visit->university_ru }}"
                                class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
                         />
                     </label>
+
                     <label class="block text-sm">
-                        <span class="text-gray-700 dark:text-gray-400">Soxasining ruscha nomi</span>
-                        <input type="text" name="specialty_ru" value="{{$about->specialty_ru}}"
-                               class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
-                        />
-                    </label>
-                    <label class="block text-sm">
-                        <span class="text-gray-700 dark:text-gray-400">Soxasining inglizcha nomi</span>
-                        <input type="text" name="specialty_en" value="{{$about->specialty_en}}"
+                        <span class="text-gray-700 dark:text-gray-400"> Description uzb version </span>
+                        <input type="text" name="description_uz" value="{{ $visit->description_uz }}"
                                class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
                         />
                     </label>
                     <label class="block mt-4 text-sm">
-                        <span class="text-gray-700 dark:text-gray-400">Pochta</span>
-                        <input type="email" name="email" value="{{$about->email}}"
+                        <span class="text-gray-700 dark:text-gray-400"> Description rus version </span>
+                        <input type="text" name="description_ru" value="{{ $visit->description_ru }}"
                                class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
                         />
                     </label>
                     <label class="block mt-4 text-sm">
-                        <span class="text-gray-700 dark:text-gray-400">Tel raqam</span>
-                        <input type="tel" name="tel" value="{{$about->tel}}"
+                        <span class="text-gray-700 dark:text-gray-400">Description eng version</span>
+                        <input type="text" name="description_en" value="{{ $visit->description_en }}"
                                class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
                         />
                     </label>
+
                     <label class="block mt-4 text-sm">
-                        <span class="text-gray-700 dark:text-gray-400">Telegram</span>
-                        <input type="text" name="telegram" value="{{$about->telegram}}"
+                        <span class="text-gray-700 dark:text-gray-400"> Date </span>
+                        <input type="date" name="date" value="{{ $visit->date }}"
                                class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
                         />
                     </label>
+
                     <label class="block mt-4 text-sm">
-                        <span class="text-gray-700 dark:text-gray-400">Instagram</span>
-                        <input type="text" name="instagram" value="{{$about->instagram}}"
-                               class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
-                        />
-                    </label>
-                    <label class="block mt-4 text-sm">
-                        <span class="text-gray-700 dark:text-gray-400">Rasim</span>
-                        <input type="file" name="image" value="{{$about->image}}"
+                        <span class="text-gray-700 dark:text-gray-400">photo</span>
+                        <input type="file" name="photo" value="{{ $visit->photo }}"
                                class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
                         />
                     </label>
@@ -88,7 +100,7 @@
                     <button type="submit"
                             class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
                     >
-                        Saqlash
+                        Save
                     </button>
                 </form>
             </div>
